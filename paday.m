bd3e64e874ddd71d057e0f3e4fec8e387cab8559
QuentinUK <QuentinUK@live.co.uk>
2017-01-15 08:37:07 +0000

    Update paday.m

--- paday.m
+++ paday.m
@@ -1,9 +1,9 @@
 function [A,B] = paday(c,p,q)
 % c is the Taylor Approximation, col vector
 % p the number of terms in the numerator A, and q the number of terms in the denominator B
-Temp = sparse(p+q+1, q+1)
+Temp = sparse(p+q+1, q+1);
 for iq = 1:q+1
-temp(iq:length(c), iq)=c(1:length(c)-iq+1)
+    temp(iq:length(c), iq)=c(1:length(c)-iq+1);
 end
-B = -Temp(p+2:p+q+1,2:q+1)\c(p+2:p+q+1)
+B = -Temp(p+2:p+q+1,2:q+1)\c(p+2:p+q+1);
 A = Temp(1:p+1,iq+1)*(1;B)
